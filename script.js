3ea0437555474b86a245bb8148928365f58be085
reyna roman <reynaroman753@gmail.com>
2023-09-22 07:29:56 -0300

    projeto concluido

---
+++ script.js
@@ -0,0 +1,40 @@
+const dia = document.getElementById('dia')
+const hora = document.getElementById('hora')
+const minuto = document.getElementById('minuto')
+const segundo = document.getElementById('segundo')
+
+const lancamento = "17 oct 2023"
+
+function countDown(){
+    const datalanc = new Date(lancamento)
+    const hoje = new Date()
+
+    const segTotal = (datalanc-hoje)/1000;
+
+    const finalDias = Math.floor (segTotal /60 /60 /24);
+    const finalHoras = Math.floor (segTotal /60 /60 )%24;
+    const finalMinutos = Math.floor (segTotal / 60) %60;
+    const finalSegundos = Math.floor (segTotal)%60;
+
+    dia.innerHTML = formatoTempo (finalDias)
+    hora.innerHTML = formatoTempo (finalHoras)
+    minuto.innerHTML = formatoTempo (finalMinutos)
+    segundo.innerHTML = formatoTempo (finalSegundos)
+
+
+    
+}
+ function formatoTempo(tempo){
+    return tempo < 10? `0${tempo}`:tempo;
+}
+
+function criarLetra() {
+    var letra = document.createElement("span");
+    letra.innerHTML = "A";
+    letra.style.float = "right";
+
+    document.getElementById("countdown").appendChild(letra);
+}
+
+countDown();
+setInterval(countDown,1000)
